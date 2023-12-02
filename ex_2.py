4e81c15603be90025095c655bb0b0f0efd41a0f0
Ainur Shamsutdinov <shamsut@gmail.com>
2023-12-03 02:44:50 +0300

    2nd exercise solution

--- ex_2.py
+++ ex_2.py
@@ -7,7 +7,7 @@ import logging
 app = FastAPI()
 
 
-films: list = list()
+movies: list = list()
 
 
 class Film(BaseModel):
@@ -17,18 +17,18 @@ class Film(BaseModel):
     genre: str = None
 
 
-films.append(Film(id=0, title='Gone with the wind', description='About the civil war', genre='drama'))
-films.append(Film(id=1, title='Fight club', description='Guy tries to survive depression', genre='thriller'))
-films.append(Film(id=2, title='Saving private Rayan', description='Saving guy whose brothers died', genre='wardrama'))
-films.append(Film(id=3, title='Forest Gump', description='Mentally challenged guy gets lucky', genre='melodrama'))
-films.append(Film(id=4, title='Pulp Fiction', description='Criminals do what criminals do', genre='criminal'))
-films.append(Film(id=5, title='Hateful Eight', description='Bad people kill each other', genre='drama'))
-films.append(Film(id=6, title='Departed', description='Double agents are looking for each other', genre='policedrama'))
-films.append(Film(id=7, title='Nocturnal animals', description='Family robbed and killed', genre='drama'))
-films.append(Film(id=8, title='Stringer', description='Sociopath gets his way into news business', genre='thriller'))
-films.append(Film(id=10, title='Edge of tomorrow', description='Tom Cruise fights aliens', genre='action'))
+movies.append(Film(id=0, title='Gone with the wind', description='About the civil war', genre='drama'))
+movies.append(Film(id=1, title='Fight club', description='Guy tries to survive depression', genre='thriller'))
+movies.append(Film(id=2, title='Saving private Rayan', description='Saving guy whose brothers died', genre='wardrama'))
+movies.append(Film(id=3, title='Forest Gump', description='Mentally challenged guy gets lucky', genre='melodrama'))
+movies.append(Film(id=4, title='Pulp Fiction', description='Criminals do what criminals do', genre='criminal'))
+movies.append(Film(id=5, title='Hateful Eight', description='Bad people kill each other', genre='drama'))
+movies.append(Film(id=6, title='Departed', description='Double agents are looking for each other', genre='policedrama'))
+movies.append(Film(id=7, title='Nocturnal animals', description='Family robbed and killed', genre='drama'))
+movies.append(Film(id=8, title='Stringer', description='Sociopath gets his way into news business', genre='thriller'))
+movies.append(Film(id=10, title='Edge of tomorrow', description='Tom Cruise fights aliens', genre='action'))
 
 
 @app.get('/film/genre/{genre}')
 async def get_film_genre(genre: str):
-    return list(filter(lambda x: (x.genre == genre), films))
+    return list(filter(lambda x: (x.genre == genre), movies))
