159db3814569d358412d98bf53fcc3e8542903f2
Ainur Shamsutdinov <shamsut@gmail.com>
2023-12-03 03:31:11 +0300

    3rd exercise solution

--- ex_3.py
+++ ex_3.py
@@ -1,10 +1,28 @@
+import logging
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/message/')
-async def read_message():
-    message = {'message': 'Hello World!'}
+users: list = list()
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    password: str
+
+
+@app.post('/user/')
+async def create_user(user: User):
+    users.append(user)
+    message = f'User created {user.name}'
+    logger.info(f'Amount of users in a base {len(users)}')
     return JSONResponse(content=message, status_code=200)
